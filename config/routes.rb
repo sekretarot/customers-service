7ac41a4ae9bad65d636dc03d63cbf527c85a4b9e
Filip Andonovski <filip@coqtail.com>
2019-01-05 21:59:45 +0100

    Moved api controler and views under api/v1, adjusted frontend call and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/customers/:id', to: 'customers#show'
-  post '/customers/:id/accounts', to: 'customers#create_account'
+  namespace :api do
+    namespace :v1 do
+      get '/customers/:id', to: 'customers#show'
+      post '/customers/:id/accounts', to: 'customers#create_account'
+    end
+  end
+
   namespace :admin do
     get '/customers/:id', to: 'customers#show'
   end
