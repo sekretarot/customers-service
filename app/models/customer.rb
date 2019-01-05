11ea20d59cd3476ab179e9293768d08bfbd6ce0d
Filip Andonovski <filip@coqtail.com>
2019-01-05 11:55:32 +0100

    Persist Customer object instead of hash objects

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,9 +2,9 @@ class Customer
 
   attr_accessor :name, :surname, :id
 
-  def initialize(init_json)
-    @name = init_json[:name]
-    @surname = init_json[:surname]
-    @id = init_json[:id]
+  def initialize(init_hash)
+    @name = init_hash[:name]
+    @surname = init_hash[:surname]
+    @id = init_hash[:id]
   end
 end
